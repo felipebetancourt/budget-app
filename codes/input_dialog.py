bd18f23367ad2d9a1d14a8249f51427fb7f8e054
Felipe Betancourt <afbp.1230@gmail.com>
2018-12-28 20:36:14 +0100

    Finished and commented check_date function

--- codes/input_dialog.py
+++ codes/input_dialog.py
@@ -25,17 +25,29 @@ def check_date(date):
 
     # date format checks
     if len(date) != 10 or date[2] != "/" or date[5] != "/":
-
         raise Exception('Something wrong with date format')
 
-    day = int(date[0:1])
-    month = int(date[3:4])
-    year = int(date[6:9])
+    day = int(date[0:2])
+    month = int(date[3:5])
+    year = int(date[6:10])
 
-    # check if months and days are alright
+    # check if year is 2009 or larger
+    if year < 2019:
+        raise Exception('You sure about the year you put?')
 
+    # According to leap_year output, set number of days in February
+    if leap_year(year=year):
+        print('you had a leap-year')
+        if month == 2:
+            if day > 28:
+                raise Exception('Wrong day for February. This is a leap year.')
 
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        if day < 1 or day > 31:
+            raise Exception('Wrong number of days for this month')
 
+    elif month in [4, 6, 9, 11]:
+        if day < 1 or day > 30:
+            raise Exception('Wrong number of days for this month')
 
-
-check_date(date="321261")
+    print("Date is alright")
