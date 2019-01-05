f17620dcd335623e29611281f3f0d284e4fa91a3
Felipe Betancourt <afbp.1230@gmail.com>
2019-01-05 18:12:41 +0100

    created generate_log. So far it  an only creadte the register file and the header if it doesn't exist

---
+++ codes/generate_log.py
@@ -0,0 +1,20 @@
+import os
+import csv
+
+
+def generate_log():
+
+    # Create folder for results if it does not exist.
+    if not os.path.exists('../logs'):
+        os.makedirs('../logs')
+
+    # Check if register file exists
+    if not os.path.isfile('../logs/register.csv'):
+
+        # If if doesn't, create it and create header.
+        with open('../logs/register.csv', 'w') as csvfile:
+            header_writer = csv.writer(csvfile, delimiter=',')
+            header_writer.writerow(['Date and time', 'ID', 'Operation'])
+
+
+generate_log()
