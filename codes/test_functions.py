47f2a58b1f57af2d383677702a10c2c3c116e468
Felipe Betancourt <afbp.1230@gmail.com>
2019-01-16 14:22:11 +0100

    Created tests for leap_year. Run successfully .

--- codes/test_functions.py
+++ codes/test_functions.py
@@ -1,10 +1,15 @@
 import unittest
-from commmon_input import *
 from input_dialog import *
+from common_input import *
 from menu_dialog import *
-from generate_log import *
+
 
 class TestFuntions(unittest.TestCase):
+
+    def test_leap_year(self):
+
+        self.assertFalse(leap_year(2018))
+        self.assertTrue(leap_year(2020))
 
 
 if __name__ == '__main__':
