878e4a6d30e305d03a05af0ad07716cc573e1cd6
Felipe Betancourt <afbp.1230@gmail.com>
2019-01-16 13:59:06 +0100

    Created file for testing functions. Gonna test the functions I've written so far.

---
+++ codes/test_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from commmon_input import *
+from input_dialog import *
+from menu_dialog import *
+from generate_log import *
+
+class TestFuntions(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
